6f6e9b6a9032d4f5583a14f3af06e6b06c26c465
marcell <ciszekmarcell@gmail.com>
2019-04-21 21:04:48 +0200

    working on graphql schema

---
+++ schema.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const {
+  GraphQLObjectType,
+  GraphQLBoolean,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLInt,
+} = require('graphql');
+
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+});
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({});

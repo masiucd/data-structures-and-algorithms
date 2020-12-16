8776c5682fdaa92d98584f67859c0c929f000e90
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-12-16 12:08:39 +0100

    with map

--- src/algorithms/sort/uniq-sort/index.ts
+++ src/algorithms/sort/uniq-sort/index.ts
@@ -9,3 +9,22 @@ export const uniqSort = (xs: number[]) => {
   }
   return res
 }
+
+// With built in Map data structure
+export const uniqSort2 = (xs: number[]) => {
+  const cache = new Map()
+
+  const res = []
+  for (let n of xs) {
+    if (!cache.has(n)) {
+      cache.set(n, true)
+      res.push(n)
+    }
+  }
+  return res
+}
+
+/**
+ * When it comes to speed this is a pretty fast algorithm, we using a Dictionary(object) which is very fast for lookups
+ * When it comes to space complexity it can be a little bit slower since it is linear O(n)
+ */

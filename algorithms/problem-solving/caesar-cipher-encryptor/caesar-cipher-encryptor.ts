43c56c9427f3ee840750909485499b89c4a5dd71
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-18 08:01:43 +0100

    feat: caesarCipherEncryptor

--- algorithms/problem-solving/caesar-cipher-encryptor/caesar-cipher-encryptor.ts
+++ algorithms/problem-solving/caesar-cipher-encryptor/caesar-cipher-encryptor.ts
@@ -1,7 +1,6 @@
 export function caesarCipherEncryptor(str: string, key: number) {
   const charsXs = [];
   const newKey = key % 26;
-
   for (const char of str) {
     charsXs.push(getNewLetter(char, newKey));
   }
@@ -11,18 +10,18 @@ export function caesarCipherEncryptor(str: string, key: number) {
 
 const getNewLetter = (char: string, key: number) => {
   // moving amount of places forward
-  // 122
-  // 96
   const newCharCode = char.charCodeAt(0) + key;
-  console.log(newCharCode, backToChar(newCharCode));
+
+  // 122 = z End of the alphabet
   return newCharCode <= 122 ? backToChar(newCharCode) : backToChar(96 + (newCharCode % 122));
 };
+
+// console.log(caesarCipherEncryptor("xyz", 2)); // zab
+
+// charCode x = 120
+// charCode y = 121
+// charCode s = 115
 
 const backToChar = (charCode: number) => {
   return String.fromCharCode(charCode);
 };
-
-console.log(caesarCipherEncryptor("xyz", 2)); // zab
-// charCode x = 120
-// charCode y = 121
-// charCode s = 115

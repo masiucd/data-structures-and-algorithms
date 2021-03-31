f982d16828e2b75e8c02e209d9f5177b24eb4efb
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-31 06:58:56 +0200

    feat: squared list with java and go

--- algorithms/problem-solving/sorted-squared-list/with-go.go
+++ algorithms/problem-solving/sorted-squared-list/with-go.go
@@ -1,14 +1,39 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
+// SortedSquaredArray func
 func SortedSquaredArray(array []int) []int {
 	xs := []int{}
 
 	for _, x := range array {
-		squared := math.Sqrt(float64(x))
+		squared := math.Pow(float64(x), 2)
 		xs = append(xs, int(squared))
 	}
 
+	sort.Ints(xs)
 	return xs
+}
+
+// SortedSquaredArray2 func
+func SortedSquaredArray2(array []int) []int {
+	xs := make([]int, len(array))
+
+	for index, num := range array {
+		xs[index] = num * num
+	}
+
+	sort.Ints(xs)
+	return xs
+}
+
+func main() {
+	xs := []int{1, 2, 4, 6, 8, 9}
+	res := SortedSquaredArray(xs)
+
+	fmt.Println(res)
 }

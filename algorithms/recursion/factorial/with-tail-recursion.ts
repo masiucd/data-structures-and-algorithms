08f9f06d4d7c0f9c6cf34789640ed431e0df72d1
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-05-20 08:40:50 +0200

    feat:factorialTailRecursion

---
+++ algorithms/recursion/factorial/with-tail-recursion.ts
@@ -0,0 +1,7 @@
+const factorialTailRecursion = (n: number, total = 1): number => {
+  if (n === 0) return total
+  console.log(n, total)
+  return factorialTailRecursion(n - 1, n * total)
+}
+
+// console.log(factorialTailRecursion(5))

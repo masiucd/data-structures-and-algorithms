f943829bb695a830011b09de67f3fae86a503516
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-06-01 09:13:36 +0200

    feat: new impl of graph

--- data-structures/graphs/simple-graph/index.ts
+++ data-structures/graphs/simple-graph/index.ts
@@ -1,52 +1,56 @@
-const hasChildren = (children: Node[]) => children.length > 0
-
-interface Node {
+const hasChildren = (children: GraphNode[]) => children.length > 0
+interface GraphNode {
   key: string
-  children: Node[]
-  addChild: (child: Node) => void
+  children: GraphNode[]
+  addChild(child: GraphNode): void
+}
+interface Graph {
+  addNode(key: string): void
+  getNode(key: string): GraphNode | boolean
+  createEdge(key1: string, key2: string): boolean
+  print(): string[]
 }
 function createNode(key: string) {
-  const children: Array<Node> = []
+  const children: GraphNode[] = []
   return {
     key,
     children,
-    addChild(node: Node) {
-      children.push(node)
+    addChild(child: GraphNode) {
+      children.push(child)
     },
   }
 }
 
 function createGraph(directed = false) {
-  const nodes: Array<Node> = []
-  const edges: Array<string> = []
+  const edges: string[] = []
+  const nodes: GraphNode[] = []
 
   return {
-    directed,
-    edges,
-    nodes,
-    addNode(nodeKey: string) {
-      nodes.push(createNode(nodeKey))
+    addNode(key: string) {
+      nodes.push(createNode(key))
     },
-    getNode(key: string) {
-      return nodes.find(node => node.key === key)
+    getNode(key: string): GraphNode | undefined {
+      return nodes.find(n => n.key === key)
     },
-    addEdge(key1: string, key2: string) {
+    createEdge(key1: string, key2: string) {
       const node1 = this.getNode(key1)
       const node2 = this.getNode(key2)
+
       if (node1 && node2) {
         node1.addChild(node2)
-        edges.push(`${key1}---${key2}`)
+        edges.push(`--${key1}--${key2}`)
       }
 
-      if (!directed && node1) {
-        node2?.addChild(node1)
+      if (!directed && node1 && node2) {
+        node2.addChild(node1)
       }
+      return true
     },
     print() {
       return nodes.map(({ key, children }) => {
         let result = key
         if (hasChildren(children)) {
-          result += ` => ${children.map(child => `${child.key}`).join(" ")}`
+          result += `${children.map(c => `${c.key}`).join(" ")}`
         }
         return result
       })
@@ -54,15 +58,15 @@ function createGraph(directed = false) {
   }
 }
 
-const g = createGraph(true) // directed
+// const g = createGraph(true) // directed
 
-g.addNode("Marcell")
-g.addNode("Filip")
-g.addNode("Jurek")
-g.addNode("Barbara")
+// g.addNode("Marcell")
+// g.addNode("Filip")
+// g.addNode("Jurek")
+// g.addNode("Barbara")
 
-g.addEdge("Marcell", "Barbara")
-g.addEdge("Filip", "Jurek")
-g.addEdge("Barbara", "Jurek")
-g.addEdge("Jurek", "Marcell")
-console.log(g.print())
+// g.addEdge("Marcell", "Barbara")
+// g.addEdge("Filip", "Jurek")
+// g.addEdge("Barbara", "Jurek")
+// g.addEdge("Jurek", "Marcell")
+// console.log(g.print())

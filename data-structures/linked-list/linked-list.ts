20a62f885479e950176c762c34c4ac0f6a21c699
masiucd <ciszekmarcell@gmail.com>
2021-07-17 15:10:22 +0200

    ll

--- data-structures/linked-list/linked-list.ts
+++ data-structures/linked-list/linked-list.ts
@@ -1,200 +1,155 @@
-class Node {
-<<<<<<< HEAD
-  value: number
-  next: Node | null
-  constructor(value: number) {
-    this.value = value
-=======
+interface Node {
   data: number
   next: Node | null
-  constructor(data: number) {
-    this.data = data
->>>>>>> 11108822c58e1bb2b822776e03cbcd1f4ac45629
-    this.next = null
-  }
 }
 
-<<<<<<< HEAD
-class List {
-=======
-class LinkedList {
->>>>>>> 11108822c58e1bb2b822776e03cbcd1f4ac45629
+interface LinkedList {
   head: Node | null
   tail: Node | null
-  #size: number
-  constructor() {
-    this.head = null
-    this.tail = null
-    this.#size = 0
-  }
-
-<<<<<<< HEAD
-  insertHead(value: number) {
-    const newNode = new Node(value)
-    if (!this.head) {
-      this.head = newNode
-      this.tail = newNode
-      this.#size++
-      return this
-    }
-    return this
-  }
-
-  prepend(value: number) {
-    if (!this.head) {
-      this.insertHead(value)
-      return this
-    }
-    const newNode = new Node(value)
-    newNode.next = this.head
-    this.head = newNode
-    this.#size++
-    return this
-  }
-  append(value: number) {
-    if (!this.head) {
-      this.insertHead(value)
-      return this
-    }
-    if (!this.tail) return this
-    const newNode = new Node(value)
-    this.tail.next = newNode
-    this.tail = newNode
-    this.#size++
-    return this
-  }
-
-  getSize(): number {
-    return this.#size
-  }
-
-  printNodes(): void {
-    const nodesList: string[] = []
-    let currentNode = this.head
-    let tracker = 0
-    while (currentNode !== null) {
-      tracker++
-      nodesList.push(
-        tracker !== this.getSize() ? `${currentNode?.value} ->` : `${currentNode?.value} `
-      )
-      currentNode = currentNode.next
-    }
-    console.log(nodesList)
-=======
-  getSize(): number {
-    return this.#size
-  }
-  incrementSize(): void {
-    this.#size++
-  }
-
-  /**
-   *
-   * @desc insert at the beginning
-   */
-  prepend(data: number): LinkedList {
-    if (!this.head) {
-      this.head = new Node(data)
-      this.tail = this.head
-      this.incrementSize()
-      return this
-    }
-    const newNode = new Node(data)
-    newNode.next = this.head
-    this.head = newNode
-    this.incrementSize()
-    return this
-  }
-
-  /**
-   *
-   * @desc push to the end of the list
-   */
-  append(data: number): LinkedList {
-    if (!this.tail) {
-      this.head = new Node(data)
-      this.tail = this.head
-      this.incrementSize()
-      return this
-    }
-    const newNode = new Node(data)
-    this.tail.next = newNode
-    this.tail = newNode
-    this.incrementSize()
-    return this
-  }
-
-  getHead() {
-    return this.head
-  }
-
-  getNode(index: number) {
-    if (index < 0 || index > this.getSize()) return null
-    if (index === 0) return this.getHead()
-    if (index === this.getSize()) return this.tail
-    let tracker = 0
-    let current = this.head
-    while (current !== null) {
-      if (tracker === index) return current
-      current = current.next
-      tracker++
-    }
-    return null
-  }
-
-  printNodes(): void {
-    const nodes: string[] = []
-    let current = this.head
-    let tracker = 0
-    while (current !== null) {
-      tracker++
-      const v = tracker < this.getSize() ? `${current.data} =>` : `${current.data}`
-      nodes.push(v)
-      current = current.next
-    }
-    console.log(nodes)
-  }
-  sumNodes(): number {
-    let sum = 0
-    let current = this.head
-    while (current !== null) {
-      sum += current.data
-      current = current.next
-    }
-    return sum
->>>>>>> 11108822c58e1bb2b822776e03cbcd1f4ac45629
-  }
-
-<<<<<<< HEAD
-  // sort() {}
-  // reverse() {}
-  // removeDuplicates(): this {
-  //   // works only if sorted
-  //   let current = this.head
-  //   let nextNode = current?.next
-  //   while (current) {
-  //     if (current.data === nextNode?.data) {
-  //       current.next = nextNode.next?.next!
-  //       this.size--
-  //     }
-  //     current = current.next
-  //     nextNode = current?.next
-  //   }
-
-  //   return this
-  // }
+  size: number
+  append(data: number): Node
+  prepend(data: number): Node
+  remove(index: number): boolean
+  removeFromTail(): boolean
+  removeFromHead(): boolean
+  show(): string
+  isEmpty(): boolean
+  get(index: number): Node | boolean
+  getSize(): number
 }
-=======
-const ll = new LinkedList()
-ll.prepend(4)
-ll.prepend(1)
-ll.prepend(100)
-ll.prepend(20)
-ll.append(24)
-ll.append(125)
-ll.append(2)
-console.log(ll.printNodes())
-console.log(ll.sumNodes())
-console.log(ll.getSize())
-console.log(ll.getNode(7))
->>>>>>> 11108822c58e1bb2b822776e03cbcd1f4ac45629
+function createNode(data: number): Node {
+  return {
+    data,
+    next: null,
+  }
+}
+
+function LinkedList(): LinkedList {
+  return {
+    head: null,
+    tail: null,
+    size: 0,
+    append(data: number) {
+      const newNode = createNode(data)
+      if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+        this.size++
+        return newNode
+      }
+
+      this.tail!.next = newNode
+      this.tail = newNode
+      this.size++
+
+      return newNode
+    },
+    prepend(data: number) {
+      const newNode = createNode(data)
+      if (!this.head) {
+        this.head = newNode
+        this.tail = newNode
+        this.size++
+        return newNode
+      }
+      newNode.next = this.head
+      this.head = newNode
+      this.size++
+      return newNode
+    },
+
+    get(index: number) {
+      if (index < 0 || index >= this.getSize()) {
+        return false
+      }
+      let counter = 0
+      let current = this.head
+      while (current) {
+        if (counter === index) {
+          return current
+        }
+        counter++
+        current = current.next
+      }
+      return false
+    },
+
+    getSize() {
+      return this.size
+    },
+
+    show() {
+      const xs: string[] = []
+      let current = this.head
+      while (current) {
+        xs.push(current.data.toString())
+        current = current.next
+      }
+      return xs.join(" => ")
+    },
+    isEmpty() {
+      return this.size === 0
+    },
+    removeFromHead() {
+      if (this.isEmpty()) return false
+      this.head = this.head!.next
+      this.size--
+      return true
+    },
+    removeFromTail() {
+      if (this.isEmpty()) return false
+      if (this.head === this.tail) {
+        this.head = null
+        this.tail = null
+        this.size--
+        return true
+      }
+      let nodePrevTail = this.head
+      while (nodePrevTail?.next !== this.tail) {
+        nodePrevTail = nodePrevTail!.next
+      }
+      nodePrevTail.next = null
+      this.size--
+      return true
+    },
+    remove(index: number) {
+      if (this.isEmpty()) return false
+      if (index < 0 || index >= this.getSize()) return false
+      if (index === 0) {
+        this.removeFromHead()
+        return true
+      }
+      if (index === this.getSize()) {
+        this.removeFromTail()
+        return true
+      }
+
+      let current = this.head
+      let prev = null
+      let counter = 0
+      while (counter < index) {
+        counter++
+        prev = current
+        current = current!.next
+      }
+
+      const deleted = current
+      prev!.next = deleted!.next
+      if (prev?.next === null) {
+        this.tail = prev
+      }
+      this.size--
+      return true
+    },
+  }
+}
+
+// const l = LinkedList()
+// l.append(2)
+// l.append(12)
+// l.append(33)
+// l.prepend(99)
+// // l.removeFromHead()
+// // l.removeFromHead()
+// l.remove(2)
+// console.log(l.show())
--- main.go
+++ main.go
@@ -1,44 +1,6 @@
 package main
 
-<<<<<<< HEAD
-import "fmt"
-
-// QueueItem struct
-type QueueItem struct {
-	items []string
-}
-
-func (q *QueueItem) New() *QueueItem {
-	q.items = make([]string, 0)
-	return q
-}
-
-func (q *QueueItem) Enqueue(s string) bool {
-	q.items = append(q.items, s)
-	return true
-}
-
-func (q *QueueItem) Size() int {
-	return len(q.items)
-}
-
-func (q *QueueItem) IsEmpty() bool {
-	return q.Size() == 0
-}
-
-func (q *QueueItem) Dequeue() *string {
-	item := q.items[0]
-	q.items = q.items[1:len(q.items)]
-	return &item
-}
-
-func (q *QueueItem) Show() {
-	for _, v := range q.items {
-		fmt.Print("=> " + v)
-	}
-=======
 import (
-	"fmt"
 	"strings"
 )
 
@@ -57,19 +19,4 @@ func isIsogram(str string) bool {
 		}
 	}
 	return true
-}
-
-func main() {
-	fmt.Println(isIsogram("six-year-old"))
-	fmt.Println(isIsogram("six year Old"))
->>>>>>> d38c50ec4871f5869d93f4318659e153770b5526
-}
-
-func main() {
-	var q QueueItem
-	q.New()
-	q.Enqueue("Go to school \n")
-	q.Enqueue("Go out \n")
-	q.Enqueue("Go for a run \n")
-	q.Show()
 }
